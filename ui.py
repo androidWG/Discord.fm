3182fb2f1b35d32ca11f936f9c25188fd9f1003a
Samuel Rodrigues <samu-rodrigues@hotmail.com>
2021-09-02 19:16:04 -0300

    Changed Eel mode to Electron

--- ui.py
+++ ui.py
@@ -1,3 +1,6 @@
-import eel
+import eel.browsers
+
+
 eel.init("web")
-eel.start("settings.html", size=(400, 400))
+eel.browsers.set_path("electron", "C:\\Users\\samu-\\Repos\\Discord.fm\\node_modules\\electron\\dist\\electron.exe")
+eel.start("settings.html", mode="electron")
