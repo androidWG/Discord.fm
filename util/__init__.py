22d0c2cea6aa5128ba040543f1f9e3d86461b5c6
Samuel Rodrigues <samu-rodrigues@hotmail.com>
2021-09-13 22:27:32 -0300

    Fixed plain Exception handler

--- util/__init__.py
+++ util/__init__.py
@@ -18,7 +18,7 @@ def resource_path(relative_path: str) -> str:
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
-    except Exception:
+    except AttributeError:
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
