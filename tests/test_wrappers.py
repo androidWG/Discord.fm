073ad37c1e98ebb78a7c6bd7ab3ec3997684446c
Samuel Rodrigues <samuelrod202@gmail.com>
2022-02-05 11:32:08 -0300

    Merged test_track_info into test_wrappers and corrected errors in tests

--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -1,8 +1,7 @@
 import pylast
 from unittest import TestCase, main
 from unittest.mock import patch, MagicMock
-import wrappers.discord_rp
-import wrappers.last_fm_user
+from wrappers import track_info, last_fm_user, discord_rp
 
 
 class TestLastFm(TestCase):
@@ -16,34 +15,35 @@ class TestLastFm(TestCase):
 
     def test_invalid_username(self):
         with self.assertRaises(ValueError):
-            wrappers.last_fm_user.LastFMUser("")
+            last_fm_user.LastFMUser("")
 
     @patch("dotenv.load_dotenv")
     @patch("os.environ.get")
-    @patch("pylast.User.get_now_playing")
-    def test_check_username(self, mock_now_playing: MagicMock, *mock):
+    @patch("util.request_handler.RequestHandler.attempt_request")
+    def test_check_username(self, mock_request_handler: MagicMock, *mock):
         """Test if the check_username function is correctly handling exceptions from pylast"""
-        mock_now_playing.side_effect = [
+        mock_request_handler.side_effect = [
             self.data,
             self.error,
             self.data,
             self.error]
 
         for name in self.usernames.keys():
-            user = wrappers.last_fm_user.LastFMUser(name)
+            user = last_fm_user.LastFMUser(name)
 
             result = user.check_username()
+            print(result)
             self.assertEqual(result, self.usernames[name])
 
-    @patch("util.request_handler.attempt_request")
-    @patch("util.track_info.TrackInfo")
+    @patch("util.request_handler.RequestHandler.attempt_request")
+    @patch("wrappers.track_info.TrackInfo")
     def test_now_playing(self, mock_track_info: MagicMock, mock_request_handler: MagicMock):
         """Test if now_playing properly handles None objects and """
         mock = MagicMock(name="TestTitle", artist="TestArtist", duration=2852)
         mock_track_info.return_value = mock
         mock_request_handler.side_effect = [None, self.data, self.data]
 
-        user = wrappers.last_fm_user.LastFMUser(list(self.usernames)[0])
+        user = last_fm_user.LastFMUser(list(self.usernames)[0])
 
         result1 = user.now_playing()
         result2 = user.now_playing()
@@ -63,7 +63,7 @@ class TestDiscordRP(TestCase):
     data1 = MagicMock(name=title, artist=artist2, duration=5005)
     data2 = MagicMock(name=title, artist=artist, duration=2852)
 
-    rp = wrappers.discord_rp.DiscordRP()
+    rp = discord_rp.DiscordRP()
 
     @patch("pypresence.Presence.update")
     def test_update_status(self, mock_update: MagicMock):
@@ -76,6 +76,37 @@ class TestDiscordRP(TestCase):
 
         with self.assertRaises(AttributeError):
             self.rp.update_status(None)
+
+
+class TestTrackInfo(TestCase):
+    title = "TestTitle"
+    artist = "TestArtist"
+    artist2 = "TestArtist2"
+    duration = 2852
+
+    data1 = pylast.Track(artist, title, None, "TestUsername")
+    data2 = pylast.Track(artist2, title, None, "TestUsername")
+
+    @patch("util.request_handler.RequestHandler.attempt_request")
+    def test_values(self, mock_request_handler: MagicMock):
+        mock_request_handler.return_value = self.duration
+
+        result = track_info.TrackInfo(self.data1)
+
+        self.assertEqual(result.name, self.title)
+        self.assertEqual(result.artist, self.artist)
+        self.assertEqual(result.duration, self.duration)
+
+    @patch("util.request_handler.RequestHandler.attempt_request")
+    def test_equal(self, mock_request_handler: MagicMock):
+        mock_request_handler.return_value = self.duration
+
+        info1 = track_info.TrackInfo(self.data1)
+        info2 = track_info.TrackInfo(self.data1)
+        info3 = track_info.TrackInfo(self.data2)
+
+        self.assertTrue(info1 == info2)
+        self.assertFalse(info1 == info3)
 
 
 if __name__ == '__main__':
