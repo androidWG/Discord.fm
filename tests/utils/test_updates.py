0f2fc030f7830505ea3aa45133c0c5c2fad7855b
Samuel Rodrigues <samuelrod202@gmail.com>
2022-02-05 23:11:29 -0300

    Update system better structured, can now handle pre-releases

--- tests/utils/test_updates.py
+++ tests/utils/test_updates.py
@@ -8,18 +8,22 @@ class TestUpdates(unittest.TestCase):
     @patch.object(util.updates, "download_asset")
     @patch("settings.Settings.get")
     @patch.object(util.updates, "get_newest_release")
-    def test_check(self, mock_newest: MagicMock, mock_get: MagicMock, *mocks):
+    def test_check(self, mock_newest: MagicMock, mock_get: MagicMock, mock_download: MagicMock, *mocks):
         mock_get.return_value = False
+        # Should return false and get_newest_release shouldn't be called because auto-updates are disabled
         self.assertFalse(util.updates.check_version_and_download())
 
         mock_get.return_value = True
         mock_newest.return_value = None
+        # Should return false since get_newest_returned an error
         self.assertFalse(util.updates.check_version_and_download())
 
         mock_newest.return_value = MagicMock()
+        # Should return true since an update was found, and download_asset should be called
         self.assertTrue(util.updates.check_version_and_download())
 
         self.assertEqual(mock_newest.call_count, 2)
+        self.assertEqual(mock_download.call_count, 1)
 
 
 if __name__ == '__main__':
--- util/updates.py
+++ util/updates.py
@@ -1,48 +1,58 @@
 import logging
 import os
 import requests
+from typing import Optional
 from packaging import version
 from settings import get_version, local_settings
 from util import arg_exists, request_handler, install
 
 
-def check_version_and_download():
+def check_version_and_download() -> bool:
     if not local_settings.get("auto_update"):
         return False
 
-    new_release = get_newest_release()
-    if new_release is not None:
-        path = download_asset(new_release)
+    latest, latest_asset = get_newest_release()
+    current = get_version(True)
+    if latest is not None and latest > current or arg_exists("--force-update"):
+        path = download_asset(latest_asset)
         install.windows.do_silent_install(path)
         return True
     else:
         return False
 
 
-def get_newest_release():
+def get_newest_release() -> Optional[tuple[version.Version, dict]]:
+    """Gets the newest release from GitHub, returned as a tuple of the version and a GitHub asset object for Windows."""
     headers = {"Accept": "application/vnd.github.v3+json",
                "User-Agent": "Discord.fm"}
 
     handler = request_handler.RequestHandler("GitHub request")
-    request = handler.attempt_request(
-        requests.get,
-        url="https://api.github.com/repos/AndroidWG/Discord.fm/releases",
-        headers=headers)
-    json = request.json()
+    if local_settings.get("pre_releases"):
+        request = handler.attempt_request(
+            requests.get,
+            url="https://api.github.com/repos/AndroidWG/Discord.fm/releases",
+            headers=headers)
+        release_list = request.json()
+        release_list.sort(key=lambda x: version.parse(x["tag_name"]), reverse=True)
+        latest = release_list[0]
+    else:
+        request = handler.attempt_request(
+            requests.get,
+            url="https://api.github.com/repos/AndroidWG/Discord.fm/releases/latest",
+            headers=headers)
+        latest = request.json()
 
-    for release in json:
-        new_version = version.parse(release["tag_name"])
-        if new_version > version.parse(get_version()) or arg_exists("--force-update"):
-            logging.info(f"Need to update, newest version: {new_version}")
-
-            for asset in release["assets"]:
-                if asset["content_type"] == "application/x-msdownload" and "setup-win" in asset["name"]:
-                    return asset
-
-    return None
+    try:
+        return version.parse(latest["tag_name"]), \
+               next(x["content_type"] == "application/x-msdownload" and "setup-win" in x["name"]
+                    for x in latest["assets"])
+    except StopIteration:
+        logging.error("Newest release doesn't include a Windows executable download")
+        return None
 
 
-def download_asset(asset: dict):
+def download_asset(asset: dict) -> str:
+    """Downloads a GitHub asset to the app's data folder and returns the full path of the file."""
     headers = {"Accept": "application/octet-stream",
                "User-Agent": "Discord.fm"}
 
