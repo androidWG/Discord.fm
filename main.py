31a7dc6da16db882220334ff2da94976c7c2e588
Samuel Rodrigues <samuelrod202@gmail.com>
2022-03-13 19:32:06 -0300

    Moved .env checking to main.py

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import sys
 import globals
 import util.process
 from app_manager import AppManager
+from os.path import isfile
 from util.log_setup import setup_logging
 from settings import get_debug, get_version
 
 if __name__ == "__main__":
     print("Application started")
     setup_logging("main")
+
+    if not isfile(util.resource_path(".env")):
+        print(".env file not found, unable to get API keys and data")
+        sys.exit()
 
     logger = logging.getLogger("discord_fm").getChild(__name__)
     logger.info(f' -------- Discord.fm version {get_version()} {"(debug mode)" if get_debug() else ""} -------- ')
