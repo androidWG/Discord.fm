7447e226241d4bc0a373475cbf0df450c4d14e58
Sam Rodrigues <sam.roliveira@proton.me>
2022-07-01 00:17:27 -0400

    Added time before closing D.FM after opening settings

--- app_manager.py
+++ app_manager.py
@@ -114,6 +114,8 @@ class AppManager:
 
     def open_settings_and_wait(self):
         util.process.open_settings()
+        # Discord.fm can take a little while to start the settings UI, so wait before closing
+        sleep(1)
         if not util.is_frozen():
             self.close()
 
