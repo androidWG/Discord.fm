4011606ad4fb4e24b6167802f5d0495fd9da7917
Samuel Rodrigues <samuelrod202@gmail.com>
2022-07-01 09:58:08 -0400

    Updated tests for LastFM wrapper

--- tests/unit/test_wrappers.py
+++ tests/unit/test_wrappers.py
@@ -89,8 +89,9 @@ class TestTrackInfo(TestCase):
     data1 = pylast.Track(artist, title, None, "TestUsername")
     data2 = pylast.Track(artist2, title, None, "TestUsername")
 
+    @patch("pylast.Track.get_cover_image")
     @patch("util.request_handler.RequestHandler.attempt_request")
-    def test_values(self, mock_request_handler: MagicMock):
+    def test_values(self, mock_request_handler: MagicMock, mock_cover: MagicMock):
         mock_request_handler.return_value = self.duration
 
         result = track_info.TrackInfo(self.data1)
@@ -99,8 +100,9 @@ class TestTrackInfo(TestCase):
         self.assertEqual(result.artist, self.artist)
         self.assertEqual(result.duration, self.duration)
 
+    @patch("pylast.Track.get_cover_image")
     @patch("util.request_handler.RequestHandler.attempt_request")
-    def test_equal(self, mock_request_handler: MagicMock):
+    def test_equal(self, mock_request_handler: MagicMock, mock_cover: MagicMock):
         mock_request_handler.return_value = self.duration
 
         info1 = track_info.TrackInfo(self.data1)
