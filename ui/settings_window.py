672eec061f2b200b7f70ffbb9a648592f2ca28b4
Sam Rodrigues <sam.roliveira@proton.me>
2022-07-31 14:54:02 -0400

    Added icon for window

--- ui/settings_window.py
+++ ui/settings_window.py
@@ -1,13 +1,11 @@
 import subprocess
 import sys
-import time
-
 import wrappers.last_fm_user
 from threading import Thread, Timer, get_ident
 from tkinter import *
 from tkinter import ttk, messagebox
 from repeat_timer import RepeatTimer
-from util import process
+from util import process, resource_path
 from settings import local_settings, get_version, get_debug
 
 SMALL_PAD = (4, 0, 4, 0)
@@ -24,6 +22,8 @@ class SettingsWindow(Tk):
         super().__init__()
         self.resizable(False, False)
         self.wm_title("Discord.fm Settings")
+        icon = Image("photo", file=resource_path("resources/settings.png"))
+        self.iconphoto(True, icon)
 
         self.root = ttk.Frame(self, padding=(12, 8))
 
