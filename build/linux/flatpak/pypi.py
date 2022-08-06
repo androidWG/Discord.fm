a4c59229efd8ccef98400f1c9adb2eca74c6769f
Sam Rodrigues <sam.roliveira@proton.me>
2022-08-06 11:41:23 -0400

    Formatted pypi.py properly

--- build/linux/flatpak/pypi.py
+++ build/linux/flatpak/pypi.py
@@ -5,31 +5,35 @@ import urllib.request
 
 
 def get_pypi_url(name: str, filename: str) -> str:
-    url = 'https://pypi.org/pypi/{}/json'.format(name)
-    print('Extracting download url for', name)
+    url = f"https://pypi.org/pypi/{name}/json"
+    print("Extracting download url for", name)
     with urllib.request.urlopen(url) as response:
-        body = json.loads(response.read().decode('utf-8'))
-        for release in body['releases'].values():
+        body = json.loads(response.read().decode("utf-8"))
+        for release in body["releases"].values():
             for source in release:
-                if source['filename'] == filename:
-                    return source['url']
-        raise Exception('Failed to extract url from {}'.format(url))
+                if source["filename"] == filename:
+                    return source["url"]
+        raise Exception(f"Failed to extract url from {url}")
 
 
 def get_tar_package_url_pypi(name: str, version: str) -> str:
-    url = 'https://pypi.org/pypi/{}/{}/json'.format(name, version)
+    url = f"https://pypi.org/pypi/{name}/{version}/json"
     with urllib.request.urlopen(url) as response:
-        body = json.loads(response.read().decode('utf-8'))
-        for ext in ['bz2', 'gz', 'xz', 'zip']:
-            for source in body['urls']:
-                if source['url'].endswith(ext):
-                    return source['url']
-        err = 'Failed to get {}-{} source from {}'.format(name, version, url)
+        body = json.loads(response.read().decode("utf-8"))
+        for ext in ["bz2", "gz", "xz", "zip"]:
+            for source in body["urls"]:
+                if source["url"].endswith(ext):
+                    return source["url"]
+        err = f"Failed to get {name}-{version} source from {url}"
         raise ValueError(err)
 
 
 def download_tar_pypi(url: str, tempdir: str) -> None:
+    file_path = os.path.join(tempdir, url.split("/")[-1])
+
+    if os.path.isfile(file_path):
+        return
+
     with urllib.request.urlopen(url) as response:
-        file_path = os.path.join(tempdir, url.split('/')[-1])
-        with open(file_path, 'x+b') as tar_file:
+        with open(file_path, "x+b") as tar_file:
             shutil.copyfileobj(response, tar_file)
