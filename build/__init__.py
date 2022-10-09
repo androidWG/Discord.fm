c439e9035942a724995037075c4ddeaab8abb9e3
Sam Rodrigues <samuelrod202@gmail.com>
2022-10-09 12:55:49 -0400

    Renamed run.py to __init__.py, removed redundant code

---
+++ build/__init__.py
@@ -0,0 +1,25 @@
+import importlib
+import os
+import platform
+import sys
+
+import base
+
+sys.path.append(os.path.abspath("src"))
+import globals
+
+
+def get_build_tool(debug: bool) -> base.BuildTool:
+    version = globals.get_version(parsed=True)
+
+    if platform.system() == "Windows":
+        module = importlib.import_module("windows")
+    elif platform.system() == "Darwin":
+        module = importlib.import_module("macos")
+    elif platform.system() == "Linux":
+        module = importlib.import_module("linux")
+    else:
+        raise NotImplementedError("System is not supported")
+
+    _build_tool = module.instance()(version=version, debug=debug)
+    return _build_tool
