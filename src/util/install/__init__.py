d58921f3f4eacbedd54baa9b3a61c4af9298d3c5
Sam Rodrigues <sam.roliveira@proton.me>
2022-10-15 19:05:27 -0400

    Changed `util.install` to not import modules directly

--- src/util/install/__init__.py
+++ src/util/install/__init__.py
@@ -1,9 +1,21 @@
+import importlib
 import os
 import platform
 import sys
 
 import util
-from util.install import macos, windows
+
+
+if platform.system() == "Windows":
+    module = importlib.import_module("util.install.windows")
+elif platform.system() == "Darwin":
+    raise NotImplementedError("macOS is not implemented yet")
+elif platform.system() == "Linux":
+    raise NotImplementedError("Linux is not implemented yet")
+else:
+    raise NotImplementedError("System is not supported")
+
+_install = module.instance()()
 
 
 def get_exe_path():
@@ -13,21 +25,12 @@ def get_exe_path():
         if util.is_frozen():
             return os.path.dirname(sys.executable)
         else:
-            if platform.system() == "Windows":
-                return windows.get_installed_exe_path()
-            elif platform.system() == "Darwin":
-                return macos.get_app_folder_and_version("Discord.fm Settings")[0]
+            _install.get_executable_path()
 
 
 def get_start_with_system():
-    if platform.system() == "Windows":
-        return windows.get_startup_shortcut()
-    else:
-        raise NotImplementedError
+    return _install.get_startup()
 
 
 def set_start_with_system(new_value: bool, exe_path: str) -> bool:
-    if platform.system() == "Windows":
-        return windows.set_startup_shortcut(new_value, exe_path)
-    else:
-        raise NotImplementedError
+    return _install.set_startup(new_value, exe_path)
---
+++ src/util/install/base.py
@@ -0,0 +1,12 @@
+class BaseInstall:
+    def get_executable_path(self):
+        pass
+
+    def get_startup(self):
+        pass
+
+    def set_startup(self, new_value: bool, exe_path: str) -> bool:
+        pass
+
+    def install(self, installer_path: str):
+        pass
