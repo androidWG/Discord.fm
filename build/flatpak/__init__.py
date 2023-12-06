a6e68ea29538e25a754dd9003c2d12f23be3dcf0
Anna Rodrigues <anna.roliveira@proton.me>
2023-12-06 01:05:09 -0500

    Enabled test build including --install

--- build/flatpak/__init__.py
+++ build/flatpak/__init__.py
@@ -19,17 +19,20 @@ class FlatpakBuildTool(BuildTool):
         app["modules"][0]["build-commands"] = packages["build-commands"]
         app["modules"][0]["sources"] = packages["sources"]
 
-        with open("build/flatpak/output.json", "w") as file:
+        manifest = self._temp("build/flatpak/output.json")
+        with open(manifest, "w") as file:
             json.dump(app, file, indent=2)
 
-    # def build(self):
-    #     commands = [
-    #         "flatpak-builder",
-    #         "dist/flatpak",
-    #         "build/flatpak/output.json",
-    #         "--force-clean",
-    #     ]
-    #     subprocess.run(commands, stdout=sys.stdout, stderr=sys.stderr)
+    def build(self):
+        commands = [
+            "flatpak-builder",
+            "dist/flatpak",
+            "build/flatpak/output.json",
+            "--force-clean",
+            "--user",
+            "--install",
+        ]  # TODO: Change out of --user --install and add exporting to repo
+        subprocess.run(commands, stdout=sys.stdout, stderr=sys.stderr)
 
 
 def instance():
