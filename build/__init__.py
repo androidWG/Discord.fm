2db4187fb42703400e4857963c300e4d465344ff
Anna Rodrigues <anna.roliveira@proton.me>
2023-12-06 00:00:10 -0500

    Separated Linux and Flatpak builds

--- build/__init__.py
+++ build/__init__.py
@@ -14,13 +14,13 @@ def get_build_tool(py_path: str, flatpak: bool = False) -> build.base.BuildTool:
     ver: Version = parse(version.get_version())
 
     if flatpak:
-        module = importlib.import_module("build.linux")
+        module = importlib.import_module("build.flatpak")
     elif platform.system() == "Windows":
         module = importlib.import_module("build.windows")
     elif platform.system() == "Darwin":
         module = importlib.import_module("build.macos")
     elif platform.system() == "Linux":
-        module = importlib.import_module("build.flatpak")
+        module = importlib.import_module("build.linux")
     else:
         raise NotImplementedError("System is not supported")
 
