808c648b3db562cfd3828e1e737b440d66922a39
neufter <neufter.dev@pm.me>
2025-02-09 15:37:18 -0500

    Added colored printing and better command output

--- setup.py
+++ setup.py
@@ -9,10 +9,24 @@ from os import path as p
 
 import build
 
+
+class Colors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    GREY = "\033[90m"
+
+
 PYINSTALLER_VER = "6.11.1"
 SYNC_CMD = "uv sync --no-binary-package pyinstaller --dev"
 PYTHON_FIND_CMD = "uv python find"
-TKINTER_MESSAGE = "tkinter is required to build and run Discord.fm. Check instructions for your OS at https://stackoverflow.com/a/25905642"
+TKINTER_MESSAGE = f"{Colors.FAIL}tkinter is required to build and run Discord.fm. Check instructions for your OS at https://stackoverflow.com/a/25905642{Colors.ENDC}"
 
 
 def _delete(path: str | os.PathLike[str]) -> None:
@@ -42,39 +56,65 @@ def _check_util(cmd: str, message: str = None) -> None:
         sys.exit(2)
 
 
+def _print_header(message: str, color=Colors.HEADER) -> None:
+    print(f"{Colors.BOLD}{color}{message}{Colors.ENDC}")
+
+
+def _print_subheader(message: str, color=Colors.HEADER) -> None:
+    print(f"{color} └ {message}{Colors.ENDC}")
+
+
 class Setup:
     current_platform = platform.system()
     python = None
 
     def __init__(self, parsed_args):
         if self.current_platform not in ["Windows", "Linux", "Darwin"]:
-            parser.exit(3, f'Platform "{self.current_platform}" is unsupported!')
+            parser.exit(
+                3,
+                f'{Colors.FAIL}Platform "{self.current_platform}" is unsupported!{Colors.ENDC}',
+            )
 
         self.no_venv = parsed_args.no_venv
         self.command = parsed_args.command
+        self.output = parsed_args.output
 
     def _find_tools(self) -> None:
-        self.python = self._run(PYTHON_FIND_CMD, echo=False)
+        _print_subheader("Finding Python binary")
+        self.python = self._run(PYTHON_FIND_CMD)
 
     def _check_package(self, pkg: str, message: str = None) -> None:
-        result = self._run(f"{self.python} -m {pkg}", use_stderr=True, echo=False)
-        if f"No module named " in result:
+        _print_subheader("Checking Python packages")
+
+        result = self._run([self.python, "-c", f'"import {pkg}"'], use_stderr=True)
+        if result == "":
+            pass
+        elif f"No module named " in result:
             print(message)
             sys.exit(4)
+        else:
+            print(f"{Colors.FAIL}Unexpected error:")
+            print(result)
+            print(f"{Colors.ENDC}")
+            sys.exit(5)
 
     def _run(
         self,
         cmd: str | list[str],
         use_stderr: bool = False,
-        echo: bool = True,
+        echo_stderr: bool = True,
+        echo_stdout: bool = True,
         **kwargs,
     ) -> str | None:
-        # print(f'Running command "{cmd}"...\n')
-
         if isinstance(cmd, str):
             command = cmd.split(" ")
         else:
             command = cmd
+
+        cmd_print = f"{Colors.GREY} ─ {" ".join(command)}{Colors.ENDC}"
+        if self.output:
+            # Don't print newline to be able to replace this line later
+            print(cmd_print, end="\r")
 
         env = os.environ.copy()
         if self.no_venv:
@@ -88,18 +128,38 @@ class Setup:
             **kwargs,
         )
 
-        lines: list[str] = []
-        out = process.stderr if use_stderr else process.stdout
-        for line in out:
-            lines.append(line.rstrip())
-            if echo:
-                print(line)
+        out_lines: list[str] = []
+        err_lines: list[str] = []
 
-        result = "\n".join(lines)
+        for line in process.stderr:
+            err_lines.append(line.rstrip())
+            if echo_stderr and self.output:
+                # Print over old line if there's output
+                if len(err_lines) <= 1 and len(out_lines) == 0:
+                    print(cmd_print.replace("─", "┬"))
+                print(f"{Colors.GREY} │ {line.rstrip()}{Colors.ENDC}")
+
+        for line in process.stdout:
+            out_lines.append(line.rstrip())
+            if echo_stdout and self.output:
+                # Same here...
+                if len(out_lines) <= 1 and len(err_lines) == 0:
+                    print(cmd_print.replace("─", "┬"))
+                print(f"{Colors.GREY} │ {line}{Colors.ENDC}")
+
+        result = "\n".join(err_lines if use_stderr else out_lines)
         process.stdout.close()
         process.wait()
 
+        # Place a newline if no output is printed so the next print doesn't overwrite the command output
+        if len(err_lines) == 0 and len(out_lines) == 0 and self.output:
+            print("\n")
+
         return result
+
+    def sync(self):
+        _print_header("Syncing")
+        self._run(SYNC_CMD)
 
     def build_pyinstaller(self) -> None:
         _check_util(
@@ -116,13 +176,13 @@ class Setup:
         )
 
     def execute_command(self) -> None:
-        self._run(SYNC_CMD)
+        self.sync()
 
         match self.command:
             case "setup":
-                print("\nSetup completed")
+                _print_header("Setup completed")
             case "build":
-                print("\nBuilding Discord.fm")
+                _print_header("Building Discord.fm")
                 self._find_tools()
                 self._check_package(
                     "tkinter",
@@ -139,15 +199,16 @@ class Setup:
                 if args.cleanup:
                     bt.cleanup()
 
-                print("\nBuild completed")
+                _print_header("Build completed", Colors.OKGREEN)
             case "run":
-                print("\nRunning main.py...")
+                _print_header("Running main.py")
                 self._find_tools()
                 self._check_package(
                     "tkinter",
                     TKINTER_MESSAGE,
                 )
 
+                # TODO: Fix no output
                 self._run(
                     [self.python, "main.py"],
                     cwd=p.abspath("src"),
@@ -166,6 +227,13 @@ if __name__ == "__main__":
         action="store_true",
         dest="no_venv",
         help="Force script to use global Python instead of uv's venv",
+    )
+    parser.add_argument(
+        "--quiet",
+        "-q",
+        action="store_false",
+        dest="output",
+        help="Hide output from commands",
     )
 
     subparsers = parser.add_subparsers(help="Command to be executed", dest="command")
@@ -201,7 +269,7 @@ if __name__ == "__main__":
         "--dirty",
         action="store_false",
         dest="cleanup",
-        help="Skips cleanup, leaving temporary files and folders.",
+        help="Skips build cleanup, leaving temporary files and folders.",
     )
     sub_build.add_argument(
         "--flatpak",
